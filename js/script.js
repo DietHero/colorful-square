2368d37cfe35f982c68c2aa970078abb408f366c
Karina Białek <karinabialek2@gmail.com>
2021-04-17 17:17:02 +0200

    color added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 const square = document.querySelector('.square')
 
-square.addEventListener {
+window.addEventListener (
     "keydown",
-    function(event) {
+    changeColor
+)
 
+function changeColor (event){
+    switch (event.key) {
+        case "ArrowUp": 
+        red()
+        break;
+        case "ArrowDown": blue()
+        break;
+        case "ArrowLeft": yellow()
+        break;
+        case "ArrowRight": green()
+        break;
+        default: break
     }
+
 }
+function red() {
+    square.setAttribute('style', 'background-color: red')
+ }
+ function blue() {
+    square.setAttribute('style', 'background-color: blue')
+ }
+ function yellow() {
+    square.setAttribute('style', 'background-color: yellow')
+ }
+function green() {
+        square.setAttribute('style', 'background-color: green')
+     }
+
