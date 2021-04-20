04b42915ff712d3a5e16f2b61c396be0f36f1ad1
Karina Białek <karinabialek2@gmail.com>
2021-04-20 16:43:43 +0200

    function move added

--- js/script.js
+++ js/script.js
@@ -1,35 +1,69 @@
 const square = document.querySelector('.square')
 
-window.addEventListener (
-    "keydown",
-    changeColor
-)
+function extractPixelsFromString (pixelsString) {
+    return Number(pixelsString.replace('px', ''))
+}
 
-function changeColor (event){
+function move(deltaX = 0, deltaY = 0) {
+    const Top = extractPixelsFromString(square.style.top)
+    const Left = extractPixelsFromString(square.style.left)  
+    square.style.top = Top + deltaY + 'px'
+    square.style.left = Left + deltaX + 'px'
+}
+   
+function moveDown(deltaTop = 10) {
+    move(0, deltaTop)
+}
+
+function moveRight(deltaLeft = 10) {
+    move(deltaLeft, 0)
+}
+function moveUp(deltaTop = 10) {
+    move(0, -deltaTop)
+}
+function moveLeft(deltaLeft = 10) {
+    move(-deltaLeft, 0)
+}
+
+function red() {
+    square.style.backgroundColor = 'red'
+}
+function blue() {
+    square.style.backgroundColor = 'blue'
+}
+function yellow() {
+    square.style.backgroundColor = 'yellow'
+}
+function green() {
+    square.style.backgroundColor = 'green'
+}
+
+
+function change(event) {
     switch (event.key) {
-        case "ArrowUp": 
-        red()
-        break;
-        case "ArrowDown": blue()
-        break;
-        case "ArrowLeft": yellow()
-        break;
-        case "ArrowRight": green()
-        break;
-        default: break
+        case "ArrowUp":
+             moveUp(), red()
+            break
+        case "ArrowDown":
+            moveDown(), blue()
+            break
+        case "ArrowLeft":
+             moveLeft(), yellow()
+            break
+        case "ArrowRight":
+             moveRight(), green()
+            break
+        case "Enter":
+            square.innerText = "UŚMIECHNIJ SIĘ" 
+        default: break 
     }
 
 }
-function red() {
-    square.setAttribute('style', 'background-color: red')
- }
- function blue() {
-    square.setAttribute('style', 'background-color: blue')
- }
- function yellow() {
-    square.setAttribute('style', 'background-color: yellow')
- }
-function green() {
-        square.setAttribute('style', 'background-color: green')
-     }
+
+    window.addEventListener(
+        "keydown",
+        change
+        )
+        
+ 
 
